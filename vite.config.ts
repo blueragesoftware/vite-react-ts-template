e8c3cd80f9e780f791bd9b2e922e6fa95a51c6a5
e.biyik <e.biyik@vk.team>
2025-04-27 14:36:20 +0300

    make root /

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/reactjs-template/',
   css: {
     preprocessorOptions: {
       scss: {
