0d29e8437031fe4f2daadc7561e7df1535166573
e.biyik <e.biyik@vk.team>
2025-06-04 22:47:24 +0300

    add components to demo

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,13 +1,24 @@
 import type { FC } from 'react';
 import { Page } from '@/components/Page.tsx';
-import styles from './IndexPage.module.css';
+import { List } from '@telegram-apps/telegram-ui';
+import { CellSection } from '@/components/CellSection.tsx';
+import { FormSection } from '@/components/FormSection/FormSection.tsx';
+import { BannerSection } from '@/components/BannerSection.tsx';
+import { TimelineSection } from '@/components/TimelineSection/TimelineSection.tsx';
+import { TooltipSection } from '@/components/TooltipSection/TooltipSection.tsx';
+import { ModalSection } from '@/components/ModalSection/ModalSection.tsx';
 
 export const IndexPage: FC = () => {
   return (
     <Page back={false}>
-      <div className={styles.container}>
-        <h1 className={styles.title}>Telegram Mini App</h1>
-      </div>
+      <List>
+        <CellSection />
+        <FormSection />
+        <BannerSection />
+        <TimelineSection />
+        <TooltipSection />
+        <ModalSection />
+      </List>
     </Page>
   );
 };
